46543fc74fc986f683ba9ca0562d1d09252d204c
Jarrod Hoddinott <jarrod.colberthoddinott@Student.SL.ON.ca>
2026-07-15 16:45:33 -0400

    Added reports table to home page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,18 +1,66 @@
 import logo from '../../assets/GananoqueFoodBank.png'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = ({PageHeader, Link, AuthenticationChecker, currentUser, updateUser}) => {
-  
+  const [inventory, setInventory] = useState([])
+
+  const loadReport = async () => {
+    try {
+      await axios.get("http://localhost:3000/inventory")
+      .then((response)=>{
+        setInventory(()=>response.data)
+      })
+    } 
+    catch(error) {
+      console.log(error.message)
+    }
+  }
+
+  loadReport()
+
   return(
     <div className="container">
       <PageHeader Link={Link} user={currentUser} />
       <AuthenticationChecker updateUser={updateUser} />
       <title>Home</title>
       
-      <h1>Home Page</h1>
-      <div className='orgInfo'>
-        <p>Established in 1987 by Audrey Jackson and the Gananoque Ministerial Committee(a group of Gananoque religious groups) and some dedicated volunteers.  The Gananoque and Area Food Bank operates on the premise that in a world where we have so much, no one should go hungry.</p>
-        <p>We are an emergency community service providing food to our community as needed and as available.</p>
-        <p>We are a registered non-profit charitable organization governed by a volunteer Board of Directors who are assisted by a group of committed volunteers. We are a non-denominational, non-political organization sustained solely by private donations and we do not receive any government funding.</p>
+      <h1>Dashboard</h1>
+      <h2>This will serve as the main landing page of the app, showing relevant top level info at a glance</h2>
+      <div>
+          <h1>Database reports</h1>
+          <select defaultValue="lowstock" onChange={loadReport}>
+            <option value="lowstock">Low Stock</option>
+          </select>
+
+          <p>Current list of items below desired stock level</p>
+
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  Item
+                </th>
+                <th>
+                  Count
+                </th>
+                <th>
+                  Par
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {inventory.map((item)=>(
+                item.stock < item.par ?
+                  <tr>
+                    <td>{item.name}</td>
+                    <td>{item.stock}</td>
+                    <td>{item.par}</td>
+                  </tr>
+                : ""
+              ))}
+            </tbody>
+          </table>
       </div>
       <img src={logo}></img> 
     </div>
